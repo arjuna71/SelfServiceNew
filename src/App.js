66f69ad9ead64e3189d7e8dfd3193d5ed8bdaedf
Mrunalini Kathoke <Mrunalini.k@inteqsolutions.com>
2023-08-23 19:43:11 +0530

    Button component created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-import ButtonGroup from './Component/ButtonGroup';
-// import Inputfield from './Component/InputField';
+import Button from './Component/Button';
 
 function App() {
-  const PrintButtonClick = (event) =>{
+  const PrintButtonClick = (event) => {
     console.log(event.target.name);
+    alert(event.target.name)
+  }
+
+  const OnClickFunction = (event) => {
+    alert(event.target.id);
   }
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonGroup button={["one", "Two", "Three"]} doSomethingAfterClick={PrintButtonClick}/>
+
       </header>
+      <body className='App-body'>
+
+      <div className='customStyleForDiv'>
+          <Button id="btnTextSpecificText" button='Text With Specific Width' doSomethingAfterClick={PrintButtonClick} size='staticButtonSize' />
+      </div>
+
+        {/* <div className='customStyleForDiv'>
+          <Button id="btnResponsiveText" button='Responsive Text' doSomethingAfterClick={PrintButtonClick} style={{ 'color': 'green' }} />
+        </div> 
+         <div className='customStyleForDiv'>
+          <Button id="btnImageText" button='Text With Image' doSomethingAfterClick={OnClickFunction}>
+            <img src={logo} id="btnImageText1" alt="buttonpng" border="0" />
+          </Button>
+        </div>
+        <div className='customStyleForDiv'>
+          <Button doSomethingAfterClick={OnClickFunction}>
+            <img className='imageSize' id="btnImage1" src={logo} />
+          </Button>
+        </div> */}
+      </body>
+      <footer className='App-footer'>
+
+      </footer>
     </div>
+
   );
 }
 
---
+++ src/Component/Button.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import './button.css'
+import logo from '../logo.svg'
+
+
+const Button = ({ button, doSomethingAfterClick, size, ...props }) => {
+    return (
+        <>
+            <button
+                type="button"
+                aria-label={button}
+                name={button}
+                onClick={doSomethingAfterClick}
+                //onClick={(event)=> {handleClick(event)}}
+                className={`customButton ${size}`}
+                {...props}
+            >
+                {props.children}
+                {button}
+                {/* {img1 !== '' ? <img src={logo} alt="buttonpng" border="0" name={`img${button}`} /> : " "} */}
+            </button>
+
+        </>
+    );
+}
+
+export default Button
+
